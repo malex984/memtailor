85e6cc5b7ee37b3997b63c1817a3ca467ae5a3e1
Bjarke Hammersholt Roune <www.broune.com>
2011-10-17 17:25:24 -0400

    Added a cpp file for including all cpp files.

---
+++ src/all.cpp
@@ -0,0 +1,5 @@
+// This implementation file includes all the implementation files needed
+// to get this library working.
+#include "BufferPool.cpp"
+#include "MemoryBlocks.cpp"
+#include "Arena.cpp"
